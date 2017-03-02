a9115e1287dc66407ad80bced709b7f0504082eb
Van Phan <v.phan@lescomptoirs.com>
2017-03-02 11:42:20 +0100

    Page Services created

---
+++ services.php
@@ -0,0 +1,193 @@
+<?php include('header.php'); ?>
+        <!-- Page Title
+		============================================= -->
+		<section id="page-title" class="page-title-parallax page-title-dark" style="padding: 250px 0; background-image: url('images/bg/bg-title.jpg'); background-size: cover; background-position: center center;" data-stellar-background-ratio="0.4">
+
+			<div class="container clearfix">
+				<h1>Nos services</h1>
+				<span>Everything you need to know about our Company</span>
+				<ol class="breadcrumb">
+					<li><a href="index.php">Accueil</a></li>
+					<li class="active">Nos services</li>
+				</ol>
+			</div>
+
+		</section><!-- #page-title end -->
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+
+			<div class="content-wrap">
+
+				<div class="container clearfix">
+
+					<div class="col_one_third">
+						<div class="feature-box fbox-center fbox-outline fbox-effect nobottomborder">
+							<div class="fbox-icon red-team">
+								<a href="#"><i class="icon-crop i-alt"></i></a>
+							</div>
+							<h3>Premium Sliders Included<span class="subtitle">About 20+ Dedicated Slider Templates</span></h3>
+						</div>
+					</div>
+
+					<div class="col_one_third">
+						<div class="feature-box fbox-center fbox-outline fbox-effect nobottomborder">
+							<div class="fbox-icon red-team">
+								<a href="#"><i class="icon-tint i-alt"></i></a>
+							</div>
+							<h3>Unlimited Color Options<span class="subtitle">16.7+ Million on your fingertips</span></h3>
+						</div>
+					</div>
+
+					<div class="col_one_third col_last">
+						<div class="feature-box fbox-center fbox-outline fbox-effect nobottomborder">
+							<div class="fbox-icon red-team">
+								<a href="#"><i class="icon-text-width i-alt"></i></a>
+							</div>
+							<h3>CUSTOMIZABLE FONTS<span class="subtitle">Unlimited Fonts &amp; Customizations</span></h3>
+						</div>
+					</div>
+
+					<div class="clear"></div>
+
+					<div class="col_one_third nobottommargin">
+						<div class="feature-box fbox-center fbox-outline fbox-effect nobottomborder">
+							<div class="fbox-icon blue-team">
+								<a href="#"><i class="icon-ok i-alt"></i></a>
+							</div>
+							<h3>12+ Header Designs<span class="subtitle">Customizable Headers &amp; Menus</span></h3>
+						</div>
+					</div>
+
+					<div class="col_one_third nobottommargin">
+						<div class="feature-box fbox-center fbox-outline fbox-effect nobottomborder">
+							<div class="fbox-icon blue-team">
+								<a href="#"><i class="icon-thumbs-up i-alt"></i></a>
+							</div>
+							<h3>Awesome Mega menu<span class="subtitle">Stylish &amp; Simple Chunky Menus</span></h3>
+						</div>
+					</div>
+
+					<div class="col_one_third nobottommargin col_last">
+						<div class="feature-box fbox-center fbox-outline fbox-effect nobottomborder">
+							<div class="fbox-icon blue-team">
+								<a href="#"><i class="icon-eye i-alt"></i></a>
+							</div>
+							<h3>Retina Ready Graphics<span class="subtitle">Crystal Clear Images &amp; Icons</span></h3>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="section">
+
+					<div class="container clear-bottommargin clearfix">
+
+						<div class="heading-block center">
+							<h2>Features List.</h2>
+							<span>We provide a wide array of Usable &amp; Flexible Features</span>
+						</div>
+
+						<div class="row">
+
+							<div class="col-md-4 bottommargin">
+								<img data-animate="fadeInLeftBig" src="images/hacker.png" alt="services" class="center-block">
+							</div>
+
+							<div class="col-md-4 col-sm-6 topmargin bottommargin">
+
+								<div class="col_full">
+
+									<div class="feature-box fbox-plain fbox-small fbox-dark">
+										<div class="fbox-icon">
+											<a href="#"><i class="icon-line-monitor"></i></a>
+										</div>
+										<h3>Responsive Layout</h3>
+										<p>Powerful Layout with Responsive functionality that can be adapted to any screen size. Resize browser to view the changes.</p>
+									</div>
+
+								</div>
+
+								<div class="col_full">
+
+									<div class="feature-box fbox-plain fbox-small fbox-dark">
+										<div class="fbox-icon">
+											<a href="#"><i class="icon-line-eye"></i></a>
+										</div>
+										<h3>Retina Ready Graphics</h3>
+										<p>Looks beautiful &amp; ultra-sharp on Retina Screen Displays. Retina Icons, Fonts &amp; all others graphics are optimized.</p>
+									</div>
+
+								</div>
+
+								<div class="col_full nobottommargin">
+
+									<div class="feature-box fbox-plain fbox-small fbox-dark">
+										<div class="fbox-icon">
+											<a href="#"><i class="icon-line-power"></i></a>
+										</div>
+										<h3>Powerful Performance</h3>
+										<p>Canvas includes tons of optimized code that are completely customizable and deliver unmatched fast performance.</p>
+									</div>
+
+								</div>
+
+							</div>
+
+							<div class="col-md-4 col-sm-6 topmargin bottommargin">
+
+								<div class="col_full">
+
+									<div class="feature-box fbox-plain fbox-small fbox-dark">
+										<div class="fbox-icon">
+											<a href="#"><i class="icon-line-layers"></i></a>
+										</div>
+										<h3>Premium Sliders Included</h3>
+										<p>Canvas included 20+ custom designed Slider Pages with Premium Sliders like Layer, Revolution, Swiper &amp; others.</p>
+									</div>
+
+								</div>
+
+								<div class="col_full">
+
+									<div class="feature-box fbox-plain fbox-small fbox-dark">
+										<div class="fbox-icon">
+											<a href="#"><i class="icon-line-star"></i></a>
+										</div>
+										<h3>Unlimited Color Options</h3>
+										<p>Change the Color Scheme of the Theme in a flash just by changing the 6-digit HEX code provided in the colors.php file.</p>
+									</div>
+
+								</div>
+
+								<div class="col_full nobottommargin">
+
+									<div class="feature-box fbox-plain fbox-small fbox-dark">
+										<div class="fbox-icon">
+											<a href="#"><i class="icon-line-anchor"></i></a>
+										</div>
+										<h3>CUSTOMIZABLE FONTS</h3>
+										<p>Use any Font you like from Google Web Fonts, Typekit or other Web Fonts. They will blend in perfectly.</p>
+									</div>
+
+								</div>
+
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+
+				<div class="promo promo-dark promo-flat promo-full bottommargin footer-stick">
+					<div class="container clearfix">
+						<h3>Call us today at <span>+91.22.57412541</span> or Email us at <span>support@canvas.com</span></h3>
+						<span>We strive to provide Our Customers with Top Notch Support to make their Theme Experience Wonderful</span>
+					</div>
+				</div>
+
+			</div>
+
+		</section><!-- #content end -->
+<?php include('footer.php'); ?>
